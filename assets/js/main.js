5b3b03d748e647f52b35d7b1c673b3567e5d3154
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2019-12-29 21:13:25 +0600

    register page and login page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,38 @@ if ($('.cookie-btn').length) {
 }
 
 
+$("#form").on('submit', function(e){
+    e.preventDefault();
+    var firstname = $('input[name="firstname"]').val();
+    if(firstname.length === 0){
+      $('input[name="firstname"]').parent().addClass('haserror');
+    }  
+    var lastname = $('input[name="lastname"]').val();
+    if(lastname.length === 0){
+      $('input[name="lastname"]').parent().addClass('haserror');
+    }
+    var email = $('input[name="email"]').val();
+    if(email.length === 0){
+      $('input[name="email"]').parent().addClass('haserror');
+    }
+    var password = $('input[name="password"]').val();
+    if(password.length === 0){
+      $('input[name="password"]').parent().addClass('haserror');
+    }
+  });
 
+
+$("#ls-form").on('submit', function(e){
+    e.preventDefault(); 
+    var email = $('input[name="email"]').val();
+    if(email.length === 0){
+      $('input[name="email"]').parent().addClass('haserror');
+    }
+    var password = $('input[name="password"]').val();
+    if(password.length === 0){
+      $('input[name="password"]').parent().addClass('haserror');
+    }
+  });
 
 
 
